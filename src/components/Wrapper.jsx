352c60a65752f267a9fe2862fa7d87f54a127e62
Alex Trzeciak <trebek08@gmail.com>
2017-01-08 01:33:34 -0800

    removed unnecessary .jsx ending

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from "./Header.jsx"; 
-import Footer from "./Footer.jsx";
+import Header from "./Header"; 
+import Footer from "./Footer";
 
 export default class Wrapper extends Component {
   render() {
